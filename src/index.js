9632f64b976b67dc26eb601dcae9d649479e3fd6
GDiaz16 <egonzalodm@gmail.com>
2020-05-21 00:41:03 -0500

    Creacion home

--- src/index.js
+++ src/index.js
@@ -2,9 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Home(props) {
+  return (
+    <div className='row'>
+      <ButtonHome />
+      <Title />
+    </div>
+
+  );
+}
+function ButtonHome(props) {
+  return (
+    <div className='column-button-home'>
+      <div className='button-home'>
+        <button onClick=''>Comenzar</button>
+      </div>
+    </div>);
+}
+
+function Title(props) {
+  return (
+    <div className='column-title-home'>
+      <div className='title-home'>
+        <h1>Hola, soy Gonzalo Diaz</h1>
+      </div>
+    </div>);
+}
+
+function Menu(props) {
+  return (
+
+    <div className='column-menu-home'>
+      <div className='menu-home'>
+        <ul>
+          <li>Sobre mi</li>
+          <li>Proyectos</li>
+          <li>Conocimientos</li>
+          <li>Contáctame</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+class Body extends React.Component {
+  render() {
+    return (
+      <div> <Home /></div>
+    );
+  }
+}
+
 
 ReactDOM.render(
-    <h1>Hola mundo</h1>,
-    document.getElementById('root')
-  );
-  
+  <Body />,
+  document.getElementById('root')
+);
