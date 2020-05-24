684bbd6ef62ef4bb1f11cda9e1c607842361b763
GDiaz16 <egonzalodm@gmail.com>
2020-05-23 22:41:56 -0500

    Updates

---
+++ precache-manifest.676503059ea139734acfb57f315a1cb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6f6a2ff3dd782260b0790e2b2aebf9",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "2144f10a76d607bacf9b",
+    "url": "/Portafolio/static/css/main.1a563d86.chunk.css"
+  },
+  {
+    "revision": "43fc27ef5dc2452e6623",
+    "url": "/Portafolio/static/js/2.25992cdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portafolio/static/js/2.25992cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2144f10a76d607bacf9b",
+    "url": "/Portafolio/static/js/main.d250b60c.chunk.js"
+  },
+  {
+    "revision": "ab6c55ce5a8bca162c10",
+    "url": "/Portafolio/static/js/runtime-main.6d965abf.js"
+  },
+  {
+    "revision": "425199dde3a35ac30ecf7dda9159a91d",
+    "url": "/Portafolio/static/media/foto_portada.425199dd.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portafolio/precache-manifest.e946b7016d64a7353bc0eca88e04c68d.js"
+  "/Portafolio/precache-manifest.676503059ea139734acfb57f315a1cb5.js"
 );
 
 self.addEventListener('message', (event) => {
